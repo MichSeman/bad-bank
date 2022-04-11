7ee09dbd465da90b1d142ac8d11cae2f3638feef
Michelle Seman <89609365+MichSeman@users.noreply.github.com>
2022-04-11 13:28:21 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComp from './components/NavbarComp';
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <NavbarComp/>
+    </div>
+  );
+}
+
+export default App;
